f79f423c065e858109a52d965dd58f9481ceba88
Corry Trout <cgtrout@hotmail.com>
2024-10-14 22:17:29 -0600

    Launcher.py - make slightly wider

--- Launcher/LauncherScript/Launcher.py
+++ Launcher/LauncherScript/Launcher.py
@@ -75,7 +75,7 @@ class ScriptLauncherApp:
     def __init__(self, root):
         self.root = root
         self.root.title("Python Script Launcher")
-        self.root.geometry("800x600")
+        self.root.geometry("1000x600")
         self.root.configure(bg=DARK_BG_COLOR)  # Dark background
 
         # Create a toolbar frame to hold buttons at the top, with flat relief
