1908131bf5ff2c677b91635d8a61225c78de58a7
Corry Trout <cgtrout@hotmail.com>
2025-01-05 02:47:17 -0700

    Test.py - add color to prints

--- Scripts/Test.py
+++ Scripts/Test.py
@@ -1,13 +1,22 @@
 import sys
 import datetime
 
+try:
+    # Import all color print functions
+    from Lib.color_print import *
+
+except ImportError:
+    print("Failed to import 'Lib.color_print'. Please ensure /Lib/color_print.py is present")
+    sys.exit(1)
+
 def greet_msfs():
     """Simple function to greet the user and show the current date and time."""
-    print("Hello, Microsoft Flight Simulator Enthusiast!")
-    print("The current date and time is:", datetime.datetime.now())
+
+    print_color("Hello, Microsoft Flight Simulator Enthusiast!", color="yellow", bold=True)
     print("Python is running from:", sys.executable)
-    
-    print("\nClick 'Run Script' to load a different script.")
+    print()
+
+    print_color("Click 'Run Script' button to load a different script.", color="green", bold=False)
     print("Load the 'virtual_pos_printer.py' script to run the virtual pop-up printer.")
     print("\nRight-click the tab for this script to close it.")
 
